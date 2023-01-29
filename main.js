d54197d15eaa2c5f8cee4d8c72db64199fdffd37
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-01-29 08:27:35 +0700

    Create functions to check if valid player move and play one round

--- main.js
+++ main.js
@@ -12,13 +12,35 @@ function getPlayerChoice() {
 }
 
 function checkPlayerChoice(playerChoice) {
-    if (playerChoice == "rock"
-    || playerChoice == "paper"
-    || playerChoice == "scissors") {
+    if (playerChoice == "rock" ||
+        playerChoice == "paper" || 
+        playerChoice == "scissors") {
         return true
     } else {
         return false
     }
 }
 
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a Draw!"
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return "You Win! Rock beats Scissors."
+        } else {
+            return  "You Lose! Paper beats Rock."
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            return "You Win! Scissors beats Paper."
+        } else {
+            return "You Lose! Rock beats Scissors."
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You Win! Paper beats Rock."
+        } else {
+            return "You Lose! Scissors beats Paper."
+        }
+    }
+}
